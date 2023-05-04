3045fe664da3166318d00b8bf0ad367a78ed4f78
FabianGenell <fabian.genell@hyperisland.se>
2023-05-04 12:42:22 +0200

    Added logout

--- client/src/components/pages/tasks/Header.jsx
+++ client/src/components/pages/tasks/Header.jsx
@@ -1,13 +1,22 @@
 import { HiMenu } from 'react-icons/hi'
 import { FaUserCircle } from 'react-icons/fa'
+import { useNavigate } from "react-router-dom"
 
 import './header.css'
 
 export default function Header() {
+
+    const navigate = useNavigate();
+
+    function logout() {
+        window.sessionStorage.clear('authToken');
+        navigate('/login');
+    }
+
     return <header className='task-header'>
         <HiMenu size={30} className='icon' />
         <div className='logout-container'>
-            <span>Log out</span>
+            <span onClick={logout}>Log out</span>
             <FaUserCircle size={30} />
         </div>
     </header>
